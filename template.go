2a6237d89ec4460075497775b344052aac6f3c17
BackAged <brotherapes@gmail.com>
2021-08-15 00:18:40 +0600

    feat: define generic, product template element

---
+++ template.go
@@ -0,0 +1,141 @@
+package instabot
+
+import "encoding/json"
+
+// Template defines template.
+type Template interface {
+	TemplateType() TemplateType
+}
+
+// TemplateType defines available template type.
+type TemplateType string
+
+// all template type.
+// generic, product template are available on instagram.
+const (
+	TemplateTypeProduct TemplateType = TemplateType("product")
+	TemplateTypeGeneric TemplateType = TemplateType("generic")
+)
+
+// TemplateDefaultAction template default action.
+type TemplateDefaultAction struct {
+	buttonType ButtonType
+	URL        string
+}
+
+// NewTemplateDefaultAction returns new TemplateDefaultAction.
+func NewTemplateDefaultAction(URL string) *TemplateDefaultAction {
+	return &TemplateDefaultAction{
+		buttonType: ButtonTypeURL,
+		URL:        URL,
+	}
+}
+
+// MarshalJSON returns json of the button.
+func (d *TemplateDefaultAction) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		Type string `json:"type"`
+		URL  string `json:"url"`
+	}{
+		Type: string(d.buttonType),
+		URL:  d.URL,
+	})
+}
+
+// GenericTemplateElement defines generic template element.
+// https://developers.facebook.com/docs/messenger-platform/instagram/features/generic-template#elements
+type GenericTemplateElement struct {
+	Title         string
+	Subtitle      string
+	ImageURL      string
+	DefaultAction *TemplateDefaultAction
+	Buttons       []Button
+}
+
+// GenericTemplateElementOption defines new GenericTemplateElement
+// instantiation optional argument.
+type GenericTemplateElementOption func(*GenericTemplateElement)
+
+// WithTemplateSubtitle sets subtitle of a GenericTemplateElement.
+func WithTemplateSubtitle(subtitle string) GenericTemplateElementOption {
+	return func(gte *GenericTemplateElement) {
+		gte.Subtitle = subtitle
+	}
+}
+
+// WithTemplateImageURL sets image url of a GenericTemplateElement.
+func WithTemplateImageURL(imageURL string) GenericTemplateElementOption {
+	return func(gte *GenericTemplateElement) {
+		gte.ImageURL = imageURL
+	}
+}
+
+// WithTemplateDefaultAction sets default action a GenericTemplateElement.
+func WithTemplateDefaultAction(URL string) GenericTemplateElementOption {
+	return func(gte *GenericTemplateElement) {
+		gte.DefaultAction = &TemplateDefaultAction{
+			buttonType: ButtonTypeURL,
+			URL:        URL,
+		}
+	}
+}
+
+// WitButton sets buttons a GenericTemplateElement,
+// a maximum of 3 buttons per element is supported.
+func WitTemplateButtons(buttons []Button) GenericTemplateElementOption {
+	return func(gte *GenericTemplateElement) {
+		gte.Buttons = buttons
+	}
+}
+
+// NewGenericTemplateElement returns new GenericTemplateElement.
+func NewGenericTemplateElement(title string, opts ...GenericTemplateElementOption) *GenericTemplateElement {
+	gte := &GenericTemplateElement{
+		Title: title,
+	}
+
+	for _, opt := range opts {
+		opt(gte)
+	}
+
+	return gte
+}
+
+// MarshalJSON marshal generic template element to JSON.
+func (e *GenericTemplateElement) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		Title         string                 `json:"title"`
+		Subtitle      string                 `json:"subtitle,omitempty"`
+		ImageURL      string                 `json:"image_url,omitempty"`
+		DefaultAction *TemplateDefaultAction `json:"default_action,omitempty"`
+		Buttons       []Button               `json:"buttons,omitempty"`
+	}{
+		Title:         e.Title,
+		Subtitle:      e.Subtitle,
+		ImageURL:      e.ImageURL,
+		DefaultAction: e.DefaultAction,
+		Buttons:       e.Buttons,
+	})
+}
+
+// ProductTemplateElement defines product template element.
+// https://developers.facebook.com/docs/messenger-platform/send-messages/template/product
+type ProductTemplateElement struct {
+	ProductID string
+}
+
+// NewProductTemplateElement returns a new product template element.
+func NewProductTemplateElement(productID string) *ProductTemplateElement {
+	return &ProductTemplateElement{
+		ProductID: productID,
+	}
+}
+
+// MarshalJSON marshal product template element to JSON.
+func (e *ProductTemplateElement) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		ProductID string `json:"id"`
+	}{
+		ProductID: e.ProductID,
+	})
+}
---
+++ template_test.go
@@ -0,0 +1,285 @@
+package instabot
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultActionJSON(t *testing.T) {
+	testCases := []struct {
+		name string
+		args *TemplateDefaultAction
+		want string
+	}{
+		{
+			name: "default action",
+			args: NewTemplateDefaultAction("<url>"),
+			want: `{
+				"type":"web_url",
+				"url":"<url>"
+			}`,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			j, err := json.Marshal(tc.args)
+			assert.NoError(t, err)
+
+			assert.JSONEq(t, tc.want, string(j))
+		})
+	}
+}
+
+func TestNewGenericTemplateElement(t *testing.T) {
+	type args struct {
+		title   string
+		options []GenericTemplateElementOption
+	}
+
+	type test struct {
+		args      args
+		afterEach func(gte *GenericTemplateElement)
+	}
+
+	testCases := map[string]func(t *testing.T) test{
+		"it should have title, when no options are given": func(t *testing.T) test {
+			return test{
+				args: args{
+					title: "title",
+				},
+				afterEach: func(gte *GenericTemplateElement) {
+					assert.Equal(t, "title", gte.Title)
+					assert.Equal(t, "", gte.Subtitle)
+					assert.Equal(t, "", gte.ImageURL)
+					assert.Nil(t, gte.DefaultAction)
+					assert.Empty(t, gte.Buttons)
+				},
+			}
+		},
+		"it should have subtitle, when subtitle is given": func(t *testing.T) test {
+			return test{
+				args: args{
+					title: "title",
+					options: []GenericTemplateElementOption{
+						WithTemplateSubtitle("subtitle"),
+					},
+				},
+				afterEach: func(gte *GenericTemplateElement) {
+					assert.Equal(t, "title", gte.Title)
+					assert.Equal(t, "subtitle", gte.Subtitle)
+					assert.Equal(t, "", gte.ImageURL)
+					assert.Nil(t, gte.DefaultAction)
+					assert.Empty(t, gte.Buttons)
+				},
+			}
+		},
+		"it should have image url, when image url is given": func(t *testing.T) test {
+			return test{
+				args: args{
+					title: "title",
+					options: []GenericTemplateElementOption{
+						WithTemplateImageURL("image url"),
+					},
+				},
+				afterEach: func(gte *GenericTemplateElement) {
+					assert.Equal(t, "title", gte.Title)
+					assert.Equal(t, "", gte.Subtitle)
+					assert.Equal(t, "image url", gte.ImageURL)
+					assert.Nil(t, gte.DefaultAction)
+					assert.Empty(t, gte.Buttons)
+				},
+			}
+		},
+		"it should have default action, when default action is given": func(t *testing.T) test {
+			return test{
+				args: args{
+					title: "title",
+					options: []GenericTemplateElementOption{
+						WithTemplateDefaultAction("url"),
+					},
+				},
+				afterEach: func(gte *GenericTemplateElement) {
+					assert.Equal(t, "title", gte.Title)
+					assert.Equal(t, "", gte.Subtitle)
+					assert.Equal(t, "", gte.ImageURL)
+					assert.Equal(t, "url", gte.DefaultAction.URL)
+					assert.Empty(t, gte.Buttons)
+				},
+			}
+		},
+		"it should have buttons, when buttons are given given": func(t *testing.T) test {
+			buttons := []Button{
+				NewURLButton("title", "url"),
+			}
+			return test{
+				args: args{
+					title: "title",
+					options: []GenericTemplateElementOption{
+						WitTemplateButtons(buttons),
+					},
+				},
+				afterEach: func(gte *GenericTemplateElement) {
+					assert.Equal(t, "title", gte.Title)
+					assert.Equal(t, "", gte.Subtitle)
+					assert.Equal(t, "", gte.ImageURL)
+					assert.Nil(t, gte.DefaultAction)
+					assert.Equal(t, buttons, gte.Buttons)
+				},
+			}
+		},
+	}
+
+	for name, fn := range testCases {
+		tt := fn(t)
+
+		t.Run(name, func(t *testing.T) {
+			gte := NewGenericTemplateElement(tt.args.title, tt.args.options...)
+
+			if tt.afterEach != nil {
+				tt.afterEach(gte)
+			}
+		})
+	}
+}
+
+func TestGenericTemplateElementJSON(t *testing.T) {
+	testCases := []struct {
+		name string
+		args *GenericTemplateElement
+		want string
+	}{
+		{
+			name: "generic template element with subtitle, image, default action, buttons",
+			args: NewGenericTemplateElement(
+				"Welcome!",
+				WithTemplateImageURL("https://petersfancybrownhats.com/company_image.png"),
+				WithTemplateSubtitle("We have the right hat for everyone."),
+				WithTemplateDefaultAction("https://petersfancybrownhats.com/view?item=103"),
+				WitTemplateButtons(
+					[]Button{
+						NewURLButton("View Website", "https://petersfancybrownhats.com"),
+						NewPostBackButton("Start Chatting", "DEVELOPER_DEFINED_PAYLOAD"),
+					},
+				),
+			),
+			want: `{
+				"title": "Welcome!",
+				"image_url": "https://petersfancybrownhats.com/company_image.png",
+				"subtitle": "We have the right hat for everyone.",
+				"default_action": {
+					"type": "web_url",
+					"url": "https://petersfancybrownhats.com/view?item=103"
+				},
+				"buttons":[
+					{
+						"type": "web_url",
+						"url": "https://petersfancybrownhats.com",
+						"title": "View Website"
+					},
+					{
+						"type": "postback",
+						"title": "Start Chatting",
+						"payload": "DEVELOPER_DEFINED_PAYLOAD"
+					}              
+				]      
+			}`,
+		},
+		{
+			name: "generic template element without button",
+			args: NewGenericTemplateElement(
+				"Welcome!",
+				WithTemplateImageURL("https://petersfancybrownhats.com/company_image.png"),
+				WithTemplateSubtitle("We have the right hat for everyone."),
+				WithTemplateDefaultAction("https://petersfancybrownhats.com/view?item=103"),
+			),
+			want: `{
+				"title": "Welcome!",
+				"image_url": "https://petersfancybrownhats.com/company_image.png",
+				"subtitle": "We have the right hat for everyone.",
+				"default_action": {
+					"type": "web_url",
+					"url": "https://petersfancybrownhats.com/view?item=103"
+				}
+			}`,
+		},
+		{
+			name: "generic template element without subtitle,button",
+			args: NewGenericTemplateElement(
+				"Welcome!",
+				WithTemplateImageURL("https://petersfancybrownhats.com/company_image.png"),
+				WithTemplateDefaultAction("https://petersfancybrownhats.com/view?item=103"),
+			),
+			want: `{
+				"title": "Welcome!",
+				"image_url": "https://petersfancybrownhats.com/company_image.png",
+				"default_action": {
+					"type": "web_url",
+					"url": "https://petersfancybrownhats.com/view?item=103"
+				}
+			}`,
+		},
+		{
+			name: "generic template element without subtitle,button, image",
+			args: NewGenericTemplateElement(
+				"Welcome!",
+				WithTemplateDefaultAction("https://petersfancybrownhats.com/view?item=103"),
+			),
+			want: `{
+				"title": "Welcome!",
+				"default_action": {
+					"type": "web_url",
+					"url": "https://petersfancybrownhats.com/view?item=103"
+				}
+			}`,
+		},
+		{
+			name: "generic template element without subtitle,button, image, default action",
+			args: NewGenericTemplateElement(
+				"Welcome!",
+			),
+			want: `{
+				"title": "Welcome!"
+			}`,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			j, err := json.Marshal(tc.args)
+			assert.NoError(t, err)
+
+			assert.JSONEq(t, tc.want, string(j))
+		})
+	}
+}
+
+
+func TestProductTemplateElementJSON(t *testing.T) {
+	testCases := []struct {
+		name string
+		args *ProductTemplateElement
+		want string
+	}{
+		{
+			name: "product template element",
+			args: NewProductTemplateElement(
+				"<PRODUCT_ID>",
+			),
+			want: `{
+				"id": "<PRODUCT_ID>"
+			}`,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			j, err := json.Marshal(tc.args)
+			assert.NoError(t, err)
+
+			assert.JSONEq(t, tc.want, string(j))
+		})
+	}
+}
