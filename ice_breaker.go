f3d66de6880c037753561315fc46b4fb6389a2c4
BackAged <brotherapes@gmail.com>
2021-08-15 00:17:45 +0600

    refactor: icebreaker json marshal

--- ice_breaker.go
+++ ice_breaker.go
@@ -1,11 +1,13 @@
 package instabot
 
+import "encoding/json"
+
 // IceBreaker defines Ice Breaker.
 // frequently asked question.
 // https://developers.facebook.com/docs/messenger-platform/instagram/features/ice-breakers
 type IceBreaker struct {
-	Question string `json:"question"`
-	Payload  string `json:"payload"`
+	Question string
+	Payload  string
 }
 
 // NewIceBreaker returns a ice breaker.
@@ -14,4 +16,15 @@ func NewIceBreaker(question string, payload string) *IceBreaker {
 		Question: question,
 		Payload:  payload,
 	}
+}
+
+// MarshalJSON returns json of the ice breaker.
+func (i *IceBreaker) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		Question string `json:"question"`
+		Payload  string `json:"payload"`
+	}{
+		Question: i.Question,
+		Payload:  i.Payload,
+	})
 }
