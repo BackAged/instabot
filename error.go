be9dca9fef10a9ccbe11caa152820407b02d5bfd
BackAged <brotherapes@gmail.com>
2021-08-16 00:50:34 +0600

    refactor: add global error comment

--- error.go
+++ error.go
@@ -3,5 +3,7 @@ package instabot
 import "errors"
 
 var (
+	// ErrMissingPageAccessToken happens when instantiating instabot
+	// with empty page access token.
 	ErrMissingPageAccessToken = errors.New("missing page access token")
 )
